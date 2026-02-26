bc296253edbb64b38b00548690dbae365e49183c
Darshan G <darshan.script@gmail.com>
2026-02-26 17:38:10 +0530

    moved ordersPage to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+
+import { Routes , Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
-import { Routes , Route } from 'react-router'
+import { OrdersPage } from './pages/OrdersPage'
 import './App.css'
+
 
 function App() {
   
 
   return (
     <Routes>
-      <Route path='/' element={<HomePage />}/>
+      <Route path='/' element={<HomePage />} />
       <Route path='/checkout' element={<CheckoutPage />} />
+      <Route path='/orders' element={<OrdersPage />} />
     </Routes>
   )
 }
