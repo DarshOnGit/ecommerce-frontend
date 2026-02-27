99cd331a7aae1dec7febb2c58c1b9f33d77d34cb
Darshan G <darshan.script@gmail.com>
2026-02-27 10:53:31 +0530

    install and use axios to fetch the data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,15 @@
+
+import axios from 'axios'
 import { Header } from "../Components/Header";
 import CheckMarkIcon from "../assets/images/icons/checkmark.png";
 import { products } from "../data/products";
 import "./HomePage.css";
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
-  .then((response)=>{
-    return response.json()
-  }).then((data)=>{
-    console.log(data);
-  })
+  axios.get('http://localhost:3000/api/products')
+    .then((response)=>{
+      console.log(response.data)
+    })
 
   return (
     <>
