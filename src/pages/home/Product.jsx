22ce31a48c6cc1d7b25e8bba0dc6c564550b41a0
Darshan G <darshan.script@gmail.com>
2026-03-02 21:46:35 +0530

    display added message after addToCart for 2s

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -6,6 +6,7 @@ import CheckMarkIcon from "../../assets/images/icons/checkmark.png";
 
 export function Product({product ,loadCart}) {
   const [quantity , setQuantity] = useState(1);
+  const [showAddedMessage,setShowAddedMessage] = useState(false);
 
   const addToCart = async () => {
           await axios.post("/api/cart-items", {
@@ -13,6 +14,12 @@ export function Product({product ,loadCart}) {
             quantity,//quantity:quantity obj destructured
           });
           await loadCart();
+
+          setShowAddedMessage(true);
+
+          setTimeout(()=>{
+            setShowAddedMessage(false);
+          },2000)
         }
 
   const selectQuantity = (event) => {
@@ -60,7 +67,7 @@ export function Product({product ,loadCart}) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart">
+      <div className="added-to-cart" style={{opacity : showAddedMessage ? 1 : 0 }}>
         <img src={CheckMarkIcon} />
         Added
       </div>
