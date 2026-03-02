2d1facb542901adb5e7b7f9182a8c34541a5f3cc
Darshan G <darshan.script@gmail.com>
2026-03-02 11:02:32 +0530

    coplete html setup for tracking page

--- src/pages/tracking/TrackingPage.jsx
+++ src/pages/tracking/TrackingPage.jsx
@@ -1,28 +1,43 @@
-import { Header } from '../../Components/Header';
-import { Link , useParams} from 'react-router';
-import { useEffect , useState } from 'react';
-import axios from 'axios';
-import dayjs from 'dayjs';
-import './TrackingPage.css'
+import { Header } from "../../Components/Header";
+import { Link, useParams } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import dayjs from "dayjs";
+import "./TrackingPage.css";
+import { DeliveryDate } from "../checkout/DeliveryDate";
 
-export function TrackingPage({cart}) {
+export function TrackingPage({ cart }) {
   const { orderId, productId } = useParams();
-  const [order , setOrder] = useState(null);
+  const [order, setOrder] = useState(null);
 
-  useEffect(()=>{
-    const fetchTrackingData = async ()=>{
-      const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+  useEffect(() => {
+    const fetchTrackingData = async () => {
+      const response = await axios.get(
+        `/api/orders/${orderId}?expand=products`,
+      );
       setOrder(response.data);
-    }
+    };
 
     fetchTrackingData();
-  },[orderId])
+  }, [orderId]);
 
-  if(!order)  return null;
-  
-  const orderProduct = order.products.find((orderProduct)=>{
-    return orderProduct.productId === productId 
+  if (!order) return null;
+
+  const orderProduct = order.products.find((orderProduct) => {
+    return orderProduct.productId === productId;
   });
+
+  const totalDeliveryTimeMs =
+    orderProduct.estimatedDeliveryTimeMs - order.orderTimeMs;
+  const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+  let deliveryPercent = (timePassedMs / totalDeliveryTimeMs) * 100;
+  if (deliveryPercent > 100) {
+    deliveryPercent = 100;
+  }
+
+  const isPreparing = deliveryPercent < 33;
+  const isShipped = deliveryPercent >= 33 && deliveryPercent < 100;
+  const isDelivered = deliveryPercent === 100;
 
   return (
     <>
@@ -37,27 +52,30 @@ export function TrackingPage({cart}) {
             View all orders
           </Link>
 
-          <div className="delivery-date">Arriving on {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}</div>
-
-          <div className="product-info">
-            {orderProduct.product.name}
+          <div className="delivery-date">
+            {deliveryPercent >= 100
+              ? `Delivered on${" "}`
+              : `Arriving on${" "}`}
+            {dayjs(orderProduct.estimatedDeliveryTimeMs).format("dddd, MMMM D")}
           </div>
+
+          <div className="product-info">{orderProduct.product.name}</div>
 
           <div className="product-info">Quantity: {orderProduct.quantity}</div>
 
-          <img
-            className="product-image"
-            src={orderProduct.product.image}
-          />
+          <img className="product-image" src={orderProduct.product.image} />
 
           <div className="progress-labels-container">
-            <div className="progress-label">Preparing</div>
-            <div className="progress-label current-status">Shipped</div>
-            <div className="progress-label">Delivered</div>
+            <div className={`progress-label ${isPreparing && 'current-status'}`}>Preparing</div>
+            <div className={`progress-label ${isShipped && 'current-status'}`}>Shipped</div>
+            <div className={`progress-label ${isDelivered && 'current-status'}`}>Delivered</div>
           </div>
 
           <div className="progress-bar-container">
-            <div className="progress-bar"></div>
+            <div
+              className="progress-bar"
+              style={{ width: `${deliveryPercent}%` }}
+            ></div>
           </div>
         </div>
       </div>
