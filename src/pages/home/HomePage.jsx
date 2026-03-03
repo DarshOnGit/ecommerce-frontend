1d5b4b52ad4a18efb76535003aab9a76f42a41f8
Darshan G <darshan.script@gmail.com>
2026-03-03 13:52:39 +0530

    create search functionality

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Header } from "../../Components/Header.jsx";
-
+import { useSearchParams } from 'react-router';
 import { ProductsGrid } from "./ProductsGrid.jsx";
 import "./HomePage.css";
 
 export function HomePage({ cart ,loadCart }) {
   const [products, setProducts] = useState([]);
 
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
+
   useEffect(() => {
     const getHomeData = async () => {
-      const response = await axios.get("/api/products");
+      const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+      const response = await axios.get(urlPath);
       setProducts(response.data);
     };
 
     getHomeData();
-  }, []);
+  }, [search]);
   return (
     <>
       <link rel="icon" href="home-favicon.png" />
